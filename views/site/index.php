40e124550edbf9d7cc149e4382e534c228d02e7b
zoluxa <zoluxa@gmail.com>
2016-06-03 10:37:31 +0300

    First push
    start developing

---
+++ views/site/index.php
@@ -0,0 +1,487 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Smartbud - building social network';
+?>
+			<!-- SLIDER -->
+
+			<section id="slider" class="fullheight" style="background:url('assets/images/demo/particles_bg.jpg')">
+
+				<span class="overlay dark-2"><!-- dark overlay [0 to 9 opacity] --></span>
+
+
+
+				<canvas id="canvas-particle" data-rgb="156,217,249"><!-- CANVAS PARTICLES --></canvas>
+
+
+
+				<div class="display-table">
+
+					<div class="display-table-cell vertical-align-middle">
+
+						
+
+						<div class="container text-center">
+
+							
+
+							<h1 class="nomargin wow fadeInUp" data-wow-delay="0.4s">
+
+								WE ARE 
+
+								<!--
+
+									TEXT ROTATOR
+
+									data-animation="fade|flip|flipCube|flipUp|spin"
+
+								-->
+
+								<span class="rotate " data-animation="fade" data-speed="1500">
+
+									SMARTBUD, GEOLOCATION SERVICE, BUILDING SOCIAL NETWORK
+
+								</span>
+
+							</h1>
+
+
+
+							<p class="lead font-lato size-30 wow fadeInUp" data-wow-delay="0.7s">
+
+								Больше <span class="countTo" data-speed="4000">10000</span> компаний предоставляющих строительные товары и услуги! 
+
+								
+
+							</p>
+
+							
+
+							<a class="btn btn-default btn-lg" href="http://smartbud.in.ua/Account/Register">Зарегистрироваться</a>
+
+
+
+						</div>
+
+
+
+					</div>
+
+				</div>
+
+
+
+			</section>
+
+			<!-- /SLIDER -->
+
+
+
+
+
+
+
+			<!-- Welcome -->
+
+			<section>
+
+				<div class="container">
+
+
+
+					<div class="text-center">
+
+						<h1>Приветствуем Вас на сайте <span>SMARTBUD!</span></h1>
+
+						<h2 class="col-sm-10 col-sm-offset-1 nomargin-bottom weight-400">SMARTBUD поможет Вам найти архитектурное бюро, строительные материалы, технику, рабочих и много другого... В общем - постройте свое жилье в несколько кликов</h2>
+
+					</div>
+
+
+
+				</div>
+
+			</section>
+
+			<!-- Welcome -->
+
+
+
+
+
+
+
+			<!-- -->
+
+			<section>
+
+				<div class="container">
+
+
+
+					<div class="row">
+
+					
+
+						<div class="col-md-5 col-sm-5">
+
+							<a class="image-hover lightbox thumbnail" href="https://www.youtube.com/watch?v=XwVqSxQLvHA" data-plugin-options='{"type":"iframe"}'>
+
+								<span class="image-hover-icon image-hover-light"><!-- image-hover-light | image-hover-dark -->
+
+									<i class="fa fa-vimeo-square"><!-- video icon --></i>
+
+								</span>
+
+								<img class="img-responsive" src="assets/images/video_pick.png" alt="smartbud">
+
+							</a>
+
+						</div>
+
+
+
+						<div class="col-md-7 col-sm-7">
+
+
+
+							<header class="margin-bottom-60">
+
+								<h2>Социальная строительная сеть SMARTBUD</h2>
+
+								<p class="lead font-lato">Незаменимая вещь для каждого человека!</p>
+
+							</header>
+
+
+
+							<p>Основной идеологией SMARTBUD является улучшенный логистический подход на каждом из этапов строительства, что непременно приводит к оптимизации затрачиваемых усилий, времени , экономии денежных средств, а также средств, затрачиваемых на рекламу торгующих организаций и частных лиц.</p>
+
+							<p>Данный сервис полезен абсолютно всем, кто хоть как-то связан со строительным процессом в целом.</p>
+
+                            <p>В любом случае, кем бы вы не были в сфере строительства, какие бы услуги и товары не предлагали и искали, знайте, Вам на портал SMARTBUD.in.ua. Который сможет не только оказать Вам помощь, но и бесплатно произвести рекламу. Потому что каждый знает, что пользователи SMARTBUD.in.ua - это актуально, это выгодно и это быстро.</p>
+
+
+
+							<a class="btn btn-default btn-lg size-15 margin-top-30 lightbox" href="https://www.youtube.com/watch?v=XwVqSxQLvHA" data-plugin-options='{"type":"iframe"}'>Посмотреть презентацию</a>
+
+
+
+						</div>
+
+
+
+					</div>
+
+
+
+				</div>
+
+			</section>
+
+			<!-- / -->
+
+
+
+
+
+
+
+			<!-- -->
+
+			<section class="alternate">
+
+				<div class="container">
+
+
+
+					<div class="row">
+
+
+
+						<!-- toggle -->
+
+						<div class="col-md-4 col-sm-4">
+
+							<div class="toggle toggle-accordion toggle-transparent toggle-bordered-full">
+
+
+
+								<div class="toggle active">
+
+									<label>ВРЕМЯ</label>
+
+									<div class="toggle-content">
+
+										<p>Не нужно тратить время на поиски и сравнение цен товаров/услуг</p>
+
+									</div>
+
+								</div>
+
+
+
+								<div class="toggle">
+
+									<label>КАЧЕСТВО</label>
+
+									<div class="toggle-content">
+
+										<p>Проверка качества заказываемого товара/услуги</p>
+
+									</div>
+
+								</div>
+
+
+
+								<div class="toggle">
+
+									<label>ЭКОНОМИЯ</label>
+
+									<div class="toggle-content">
+
+										<p>Экономить покупая у производителя, а не посредника</p>
+
+									</div>
+
+								</div>
+								
+								<div class="toggle">
+
+									<label>СКИДКА</label>
+
+									<div class="toggle-content">
+
+										<p>Скидка от компаний – партнёров SMARTBUD</p>
+
+									</div>
+
+								</div>
+
+								<div class="toggle">
+
+									<label>ОБЗОР ТОВАРА/УСЛУГИ</label>
+
+									<div class="toggle-content">
+
+										<p>Краткий видео обзор товара/услуги</p>
+
+									</div>
+
+								</div>
+
+
+							</div>
+
+						</div>
+
+						<!-- /toggle -->
+
+
+
+						<!-- skills -->
+
+						<div class="col-md-4 col-sm-4">
+
+							<h4>Для поставщика</h4>
+
+
+
+							<label>
+
+								<span class="pull-right">60%</span>
+
+								Увеличение продаж по выгодному местоположению на
+
+
+							</label>
+
+							<div class="progress progress-xxs">
+
+								<div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%; min-width: 2em;"></div>
+
+							</div>
+
+
+
+							<label>
+
+								<span class="pull-right">88%</span>
+
+								Экономить на рекламе до 
+
+
+							</label>
+
+							<div class="progress progress-xxs">
+
+								<div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 88%; min-width: 2em;"></div>
+
+							</div>
+
+
+
+							<label>
+
+								<span class="pull-right">93%</span>
+
+								Повышение позиций сайта в поисковых системах 
+
+							</label>
+
+							<div class="progress progress-xxs">
+
+								<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 93%; min-width: 2em;"></div>
+
+							</div>
+
+
+
+							<label>
+
+								<span class="pull-right">77%</span>
+
+								Привлечение покупателей по видео обзору товара/услуги
+
+							</label>
+
+							<div class="progress progress-xxs">
+
+								<div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 77%; min-width: 2em;"></div>
+
+							</div>
+
+
+
+							<label>
+
+								<span class="pull-right">99%</span>
+
+								Покупка товара из-за выгодного ценового предложения
+
+							</label>
+
+							<div class="progress progress-xxs">
+
+								<div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 99%; min-width: 2em;"></div>
+
+							</div>
+
+
+
+						</div>
+
+						<!-- /skills -->
+						
+						<!-- toggle -->
+
+						<div class="col-md-4 col-sm-4">
+
+							<div class="toggle toggle-accordion toggle-transparent toggle-bordered-full">
+
+
+
+								<div class="toggle active">
+
+									<label>ИНФОРМАЦИЯ</label>
+
+									<div class="toggle-content">
+
+										<p>Получение информации о новых тендерах, о проводимых акциях и новых товарах в строительстве</p>
+
+									</div>
+
+								</div>
+
+
+
+								<div class="toggle">
+
+									<label>ОПОВЕЩЕНИЕ</label>
+
+									<div class="toggle-content">
+
+										<p>Получение сообщения по запросу товара/услуги (e’mail, sms, viber...)</p>
+
+									</div>
+
+								</div>
+
+
+
+								<div class="toggle">
+
+									<label>БАЗА</label>
+
+									<div class="toggle-content">
+
+										<p>Информация о покупателях в его сфере деятельности</p>
+
+									</div>
+
+								</div>
+								
+							</div>
+
+						</div>
+
+						<!-- /toggle -->
+
+					</div>
+
+
+
+				</div>
+
+			</section>
+
+			<!-- / -->
+
+
+			<!-- CALLOUT -->
+
+			<div class="alert alert-transparent bordered-bottom nomargin">
+
+				<div class="container">
+
+
+
+					<div class="row">
+
+
+
+						<div class="col-md-9 col-sm-12"><!-- left text -->
+
+							<h3>Зарегистрируйся сейчас и получи скидку 10% на товары/услуги от партнеров SMARTBUD!</h3>
+
+							<p class="font-lato weight-300 size-20 nomargin-bottom">
+
+								Мы дорожим каждым Вашим посещением и делаем все возможное чтобы Вам было хорошо!
+
+							</p>
+
+						</div><!-- /left text -->
+
+
+
+						
+
+						<div class="col-md-3 col-sm-12 text-right"><!-- right btn -->
+
+							<a href="http://smartbud.in.ua/Account/Register" rel="nofollow" target="_blank" class="btn btn-primary btn-lg">ЗАРЕГИСТРИРОВАТЬСЯ</a>
+
+						</div><!-- /right btn -->
+
+
+
+					</div>
+
+
+
+				</div>
+
+			</div>
+
+			<!-- /CALLOUT -->
